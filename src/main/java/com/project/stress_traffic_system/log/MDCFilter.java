2dfae644f2b454df7942049577df4040a9bb70df
Hyeunseung <hyeunseung03@gmail.com>
2023-02-13 23:49:18 +0900

    MP-49 로그 포맷 커스텀
    로그 포맷 커스텀하였습니다. 일단 콘솔에서 보이는 부분만 수정하였으며, 실제 로그 파일 롤링되는 것은 팀 내 협의 후 설정될 수 있도록 하겠습니다.

---
+++ src/main/java/com/project/stress_traffic_system/log/MDCFilter.java
@@ -0,0 +1,28 @@
+package com.project.stress_traffic_system.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jboss.logging.MDC;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.UUID;
+
+
+@Slf4j
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class MDCFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        final String trxId = UUID.randomUUID().toString().substring(0,8);
+        MDC.put("request_id", trxId);
+
+        filterChain.doFilter(servletRequest, servletResponse);
+
+        MDC.clear();
+    }
+}
