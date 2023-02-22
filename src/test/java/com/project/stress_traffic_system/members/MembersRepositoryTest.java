8a0db587304d36acf85bd67ec2d061b754dd410e
anwonseok <anwonseok72@gmail.com>
2023-02-22 09:17:20 +0900

    MP-60 회원가입/로그인 테ì수정¤트 코드

--- src/test/java/com/project/stress_traffic_system/members/MembersRepositoryTest.java
+++ src/test/java/com/project/stress_traffic_system/members/MembersRepositoryTest.java
@@ -11,11 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//@DataJpaTest //h2 DB가 default 값
+//@DataJpaTest //h2 DB가 default
 @SpringBootTest
 public class MembersRepositoryTest {
 
@@ -23,49 +22,53 @@ public class MembersRepositoryTest {
     private MembersRepository membersRepository;
 
     @Mock(name = "USERS")
-    private Members member;
+    private Members MockMember;
 
+    @Mock(name = "USERS")
+    private Members savedMockMember;
+
+    //Mock 엔티티를 사용하기 때문에 실제DB에 저장되지 않는다.
     @DisplayName("멤버가 DB에 저장이 잘 되는지 확인")
     @Test
     public void testSave() {
         //given
-        String username = "qwertdf2";
+        String username = "usertest";
         String password = "abcde123?";
         String address = "서울";
         MembersRoleEnum role = MembersRoleEnum.MEMBER;
 
-        member = new Members(username, password, address, role);
+        MockMember = new Members(username, password, address, role);
 
         // when
-        Members savedMember = membersRepository.save(member);
+        Members savedMember = membersRepository.save(MockMember);
 
         // then
         assertThat(savedMember.getId()).isNotNull();
-        assertThat(savedMember.getUsername()).isEqualTo(member.getUsername());
-        assertThat(savedMember.getPassword()).isEqualTo(member.getPassword());
-        assertThat(savedMember.getAddress()).isEqualTo(member.getAddress());
-        assertThat(savedMember.getRole()).isEqualTo(member.getRole());
+        assertThat(savedMember.getUsername()).isEqualTo(MockMember.getUsername());
+        assertThat(savedMember.getPassword()).isEqualTo(MockMember.getPassword());
+        assertThat(savedMember.getAddress()).isEqualTo(MockMember.getAddress());
+        assertThat(savedMember.getRole()).isEqualTo(MockMember.getRole());
     }
 
     @Test
     @DisplayName("저장된 멤버가 제대로 조회되는지 확인")
     void findMember() {
         //given
-        String username = RandomStringUtils.randomAlphanumeric(10);
+        String username = "asdf1234";
         String password = "abcde123?";
         String address = "서울";
         MembersRoleEnum role = MembersRoleEnum.MEMBER;
 
-        Members savedMember = membersRepository.save(new Members(username, password, address, role));
+        savedMockMember = membersRepository.save(new Members(username, password, address, role));
 
         // when
-        Members findMember = membersRepository.findById(savedMember.getId())
-                .orElseThrow(() -> new IllegalArgumentException("Wrong MemberId:<" + savedMember.getId() + ">"));
+        Members findMember = membersRepository.findById(savedMockMember.getId())
+                .orElseThrow(() -> new IllegalArgumentException("Wrong MemberId:<" + savedMockMember.getId() + ">"));
 
         // then
-        assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
-        assertThat(findMember.getPassword()).isEqualTo(savedMember.getPassword());
-        assertThat(findMember.getAddress()).isEqualTo(savedMember.getAddress());
-        assertThat(findMember.getRole()).isEqualTo(savedMember.getRole());
+        assertThat(findMember.getUsername()).isEqualTo(savedMockMember.getUsername());
+        assertThat(findMember.getPassword()).isEqualTo(savedMockMember.getPassword());
+        assertThat(findMember.getAddress()).isEqualTo(savedMockMember.getAddress());
+        assertThat(findMember.getRole()).isEqualTo(savedMockMember.getRole());
     }
 }
