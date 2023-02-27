5e0f7c03fdfe552be65bf3982892555337fa1176
Hyeunseung <hyeunseung03@gmail.com>
2023-02-27 14:10:02 +0900

    MP-63 주문 api pessimistic lock / redisson lock으로 분할

--- src/main/java/com/project/stress_traffic_system/config/EmbeddedRedisConfig.java
+++ src/main/java/com/project/stress_traffic_system/config/EmbeddedRedisConfig.java
@@ -1,43 +1,43 @@
-//package com.project.stress_traffic_system.config;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Profile;
-//import redis.embedded.RedisServer;
-//
-//import javax.annotation.PostConstruct;
-//import javax.annotation.PreDestroy;
-//import java.io.IOException;
-//
-////embedded redis는 profile이 local일 때에만 작동
-////application.properties에서 spring.profiles.active=local로 설정해 두었음
-//@Slf4j
-//@Profile("local")
-//@Configuration
-//public class EmbeddedRedisConfig {
-//
-//    //application.properties에서 포트 값 가져와서 redisPort에 저장
-//    @Value("${spring.redis.port}")
-//    private int redisPort;
-//
-//    private RedisServer redisServer;
-//
-//    //객체가 생성될 때 실행됨
-//    @PostConstruct
-//    public void redisServer() throws IOException {
-//        redisServer = RedisServer.builder()
-//                .port(redisPort)
-//                .setting("maxmemory 128M")
-//                .build();
-//        redisServer.start();
-//    }
-//
-//    //객체가 삭제될 때 실행됨
-//    @PreDestroy
-//    public void stopRedis() {
-//        if (redisServer != null) {
-//            redisServer.stop();
-//        }
-//    }
-//}
+package com.project.stress_traffic_system.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.IOException;
+
+//embedded redis는 profile이 local일 때에만 작동
+//application.properties에서 spring.profiles.active=local로 설정해 두었음
+@Slf4j
+@Profile("local")
+@Configuration
+public class EmbeddedRedisConfig {
+
+    //application.properties에서 포트 값 가져와서 redisPort에 저장
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    //객체가 생성될 때 실행됨
+    @PostConstruct
+    public void redisServer() throws IOException {
+        redisServer = RedisServer.builder()
+                .port(redisPort)
+                .setting("maxmemory 128M")
+                .build();
+        redisServer.start();
+    }
+
+    //객체가 삭제될 때 실행됨
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+}
--- src/main/java/com/project/stress_traffic_system/facade/RedissonLockStockFacade.java
+++ src/main/java/com/project/stress_traffic_system/facade/RedissonLockStockFacade.java
@@ -1,5 +1,8 @@
 package com.project.stress_traffic_system.facade;
 
+import com.project.stress_traffic_system.members.entity.Members;
+import com.project.stress_traffic_system.order.model.dto.OrderDto;
+import com.project.stress_traffic_system.order.model.dto.OrderRequestDto;
 import com.project.stress_traffic_system.order.service.OrderService;
 import com.project.stress_traffic_system.product.repository.ProductRedisRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +11,7 @@ import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Component
@@ -36,5 +40,27 @@ public class RedissonLockStockFacade {
         }
     }
 
+    public OrderDto orderOne(Members member, OrderRequestDto requestDto) {
+        RLock lock = redissonClient.getLock(requestDto.getProductId().toString());
+
+        try {
+            //얼마만큼이나 lock을 점유하고 있을 것인지
+            //현재 점유시간 1초
+            boolean available = lock.tryLock(5, 1, TimeUnit.SECONDS);
+            if (!available) {
+                log.info("lock획득 실패");
+                return null;
+            }
+
+            return orderService.orderOne(member, requestDto);
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }finally {
+            if (lock != null && lock.isHeldByCurrentThread()){
+                lock.unlock();
+            }
+        }
+    }
 }
 
